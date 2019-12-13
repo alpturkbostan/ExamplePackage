af680f94138e78bec936b2fb340a8ac678684a27
alpturk.bostan <alpturk.bostan@n11.com>
2019-12-13 10:00:34 +0300

    Initial Commit

---
+++ Sources/ExamplePackage/BasicClass.swift
@@ -0,0 +1,18 @@
+//
+//  BasicClass.swift
+//  SPMDemo
+//
+//  Created by alpturk.bostan on 6.12.2019.
+//  Copyright © 2019 alpturk.bostan. All rights reserved.
+//
+
+import UIKit
+
+public class BasicClass: NSObject {
+    
+    public static func welcome()
+    {
+        print("Welcome bro")
+    }
+    
+}
